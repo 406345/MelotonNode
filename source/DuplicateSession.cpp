edea7189982ab9dfa2272a824640bd5ee7a180f1
Shubo Yang <yangshubo@outlook.com>
2016-04-15 09:56:59 +0800

    add sync block message

--- source/DuplicateSession.cpp
+++ source/DuplicateSession.cpp
@@ -1,6 +1,9 @@
 #include "DuplicateSession.h"
 #include <MessageDuplicateDataRequest.pb.h>
 #include <BlockHub.h>
+#include <MRT.h>
+#include <MasterSession.h>
+#include <MessageSyncBlock.pb.h>
 
 DuplicateSession::DuplicateSession()
 {
@@ -51,7 +54,15 @@ void DuplicateSession::AcceptBlock( uptr<MessageDuplicateData> msg )
 
     if ( msg->islast() )
     {
-        BlockHub::Instance()->SyncBlock( this->index_ );
+        auto sync = make_uptr   ( MessageBlockMeta );
+        sync->set_fileoffset    ( this->index_->FileOffset );
+        sync->set_index         ( this->index_->Index );
+        sync->set_partid        ( this->index_->PartId );
+        sync->set_path          ( this->index_->Path );
+        sync->set_size          ( this->index_->Size );
+        sync->set_status        ( 0 );
+        MasterSession::Instance ()->SendMessage( move_ptr( sync ) );
+
         this->Close();
         return;
     }
