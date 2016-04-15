29e7b9725f52cc43f04eefe37e162f4e238d1baa
Shubo Yang <yangshubo@outlook.com>
2016-04-15 12:42:25 +0800

    update framework

---
+++ linux_update_framework.sh
@@ -0,0 +1,3 @@
+copy ..\MaratonFramework\inc\*.* 3rd\
+copy ..\MaratonFramework\project\x64\Debug\*.lib lib\ 
+pause
--- win_update_framework.bat
+++ win_update_framework.bat
@@ -1,4 +1,3 @@
 copy ..\MaratonFramework\inc\*.* 3rd\
-copy ..\MaratonFramework\project\x64\Debug\*.lib lib\
-copy ..\MaratonFramework\project\x64\Debug\*.pdb lib\
+copy ..\MaratonFramework\project\x64\Debug\*.lib lib\ 
 pause
