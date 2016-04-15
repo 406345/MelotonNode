5e92e070c7bc35d1167dc0f0690a672d5b313b44
shuboyang <shuboyang@yhgenomics.com>
2016-04-15 18:01:43 +0800

    update libuv

--- linux_build.sh
+++ linux_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf cmakebuild
-CMaker project:MelotonNode compiler:clang++ head:*.h src:*.cpp,*.c src:*.cc,*.cpp,*.hpp lib:lib/libMaratonFramework.a,lib/libuv.a,lib/libprotobuf.a flag:-std=c++11-pthread out:exe 
+CMaker project:MelotonNode compiler:clang++ head:*.h src:*.cpp,*.c src:*.cc,*.cpp,*.hpp lib:lib/libMaratonFramework.a,lib/libuv.a,lib/libprotobuf.a flag:-std=c++11-pthread out:exe debug:true 
 cd cmakebuild
 cmake ./
 make -j 4
